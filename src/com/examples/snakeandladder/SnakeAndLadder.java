761b5ae16add81f6d9864ee1fd204c5cad3951be
“akshara-phalake” <akshara.phalake18@gmail.com>
2022-03-03 12:07:02 +0530

    Added: UC1 - Start position at 0

---
+++ src/com/examples/snakeandladder/SnakeAndLadder.java
@@ -0,0 +1,10 @@
+package com.examples.snakeandladder;
+
+public class SnakeAndLadder {
+    public static void main(String[] args){
+        System.out.println("Snake and Ladder Simulator");           //welcome message
+        System.out.println("-------------------------------");
+        int startPosition = 0;
+        System.out.println("Player is starting at: " + startPosition);
+    }
+}
